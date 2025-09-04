79eda8f085a51309c6a855c7cd6b7a3724fec513
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-04 10:29:09 +0300

    Basics - Variables

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// This is my first JavaScript code!
-console.log(`Hello World!`);
+let name = `Mosh`;
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case sensitive
+
+let firstName = `Mosn`;
+let lastName = `Hamedani`;
