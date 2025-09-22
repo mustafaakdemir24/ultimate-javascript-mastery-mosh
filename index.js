d7fe112fcdc1a4b6edcc35573bc5200b8a03bf9f
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-22 11:21:41 +0300

    Operators - Ternary Operator

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Strict Equality (Type + Value)
-console.log(1 === 1);
-console.log("1" === 1);
+// If customers hava more than 100 points,
+// they are 'gold' customers, otherwise,
+// they are 'silver' customer.
+let point = 90;
+let type = point > 100 ? "gold" : "silver";
 
-// Lose Equality (Value)
-console.log(1 == 1);
-console.log("1" == 1);
-console.log(true == 1);
+console.log(type);
