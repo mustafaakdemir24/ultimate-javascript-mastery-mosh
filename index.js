c39a9eb6b74a9fd318a9aaeddb2fcaefc4196f4e
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-22 11:13:01 +0300

    Operators - Equality Operators

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-let x = 1;
+// Strict Equality (Type + Value)
+console.log(1 === 1);
+console.log("1" === 1);
 
-// Relational
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
-
-// Equality
-console.log(x === 1);
-console.log(x !== 1);
+// Lose Equality (Value)
+console.log(1 == 1);
+console.log("1" == 1);
+console.log(true == 1);
