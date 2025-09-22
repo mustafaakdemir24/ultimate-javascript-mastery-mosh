6eac84794bf99caa9ebce0cab8d3d580683fa871
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-22 11:54:56 +0300

    Operators - Logical Operators with Non-booleans

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// Logical OR (||)
-// Returns TRUE if one of the operands is TRUE
-let highIncome = false;
-let goodCreditScore = false;
-let eligibleForLoan = highIncome || goodCreditScore;
-console.log(`Eligible ${eligibleForLoan}`);
+// Falsy (false)
+// undefined
+// null
+// 0
+// false
+// '
+// NaN
 
-// NOT (!)
-let applicationRefused = !eligibleForLoan;
-console.log(`Application Refused ${applicationRefused}`);
+let userColor = "red";
+let defaultColor = "blue";
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
