1643f4202de1c40515817fef3fdf338c3f914b54
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-29 23:08:48 +0300

    Control Flow - For loop

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let role = "guest";
-
-switch (role) {
-  case "guest":
-    console.log("Guest User");
-    break;
-
-  case "moderator":
-    console.log("Moderator User");
-    break;
-
-  default:
-    console.log("Unknown User");
+for (let i = 5; i >= 1; i--) {
+  if (i % 2 !== 0) console.log(i);
 }
