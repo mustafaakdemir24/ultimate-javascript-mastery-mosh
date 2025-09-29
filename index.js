c4be962a420fb397c2c5b7c936e6886027e06c81
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-29 22:57:13 +0300

    Control Flow - Switch-Case

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// Hour
-// If hour is between 6am and 12pm: Good morning!
-// If it is between 12pm and 6pm: Good afternoon!
-// Otherwise: Good evening!
+let role = "guest";
 
-let hour = 20;
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
 
-if (hour >= 6 && hour < 12) {
-  console.log("Good morning!");
-} else if (hour >= 12 && hour < 18) {
-  console.log("Good afternoon!");
-} else console.log("Good evening!");
+  case "moderator":
+    console.log("Moderator User");
+    break;
+
+  default:
+    console.log("Unknown User");
+}
