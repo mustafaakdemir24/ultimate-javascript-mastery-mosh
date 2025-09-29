c9aa02f7cd16cf0cb99cce7e89364f196bb701e3
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-09-29 23:24:21 +0300

    Control Flow - Do-While loop

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-for (let i = 5; i >= 1; i--) {
-  if (i % 2 !== 0) console.log(i);
-}
+// for (let i = 5; i >= 1; i--) {
+//   if (i % 2 !== 0) console.log(i);
+// }
 
-let i = 0;
-while (i <= 5) {
+// let i = 0;
+// while (i <= 5) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
+
+let i = 9;
+do {
   if (i % 2 !== 0) console.log(i);
   i++;
-}
+} while (i <= 5);
