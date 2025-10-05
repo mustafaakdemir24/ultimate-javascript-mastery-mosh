aa14ba2c6c373a32bac1f44b996d17854daad82c
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-05 14:29:06 +0300

    Control Flow - Exercise 3-FizzBuzz

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const isLandscape = (width, height) => {
-  return width > height;
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by 3 and 5 => FizzBuzz
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+const fizzBuzz = (input) => {
+  if (typeof input !== "number") return NaN;
+
+  if (input % 3 === 0) {
+    if (input % 5 === 0) return "FizzBuzz";
+    return "Fizz";
+  }
+  if (input % 5 === 0) return "Buzz";
+  return input;
 };
 
-console.log(isLandscape(112, 180));
+console.log(fizzBuzz(false));
