161bc10c50ea168e0e9ae8142c270ed44abe1376
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-15 20:42:57 +0300

    Objects - Constructor Functions

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const createCircle = (radius) => {
   };
 };
 
-const circle1 = createCircle(1);
-console.log(circle1);
+const myCircle = createCircle(1);
 
-const circle2 = createCircle(2);
-console.log(circle2);
+// Constructor Function
+const Circle = (radius) => {
+  this.radius = radius;
+  this.draw = () => {
+    console.log("Draw!");
+  };
+};
+
+const circle = new Circle(1);
