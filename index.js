d9bb2bdb555d6b2783e249433ff98de8e18bbad6
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-15 13:56:10 +0300

    Control Flow - Exercise 10-Stars

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const marks = [80, 80, 50];
-
-// 0 - 59: F
-// 60 - 59: D
-// 70 - 79: C
-// 80 - 89: B
-// 90 - 100: A
-
-const calculateGrade = (marks) => {
-  const average = calculateAverage(marks);
-
-  if (average < 60) return "F";
-  if (average < 70) return "D";
-  if (average < 80) return "C";
-  if (average < 90) return "D";
-  return "A";
+const showStarts = (rows) => {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = "";
+    for (let i = 0; i < row; i++) pattern += "*";
+    console.log(pattern);
+  }
 };
 
-const calculateAverage = (array) => {
-  let sum = 0;
-  for (let item of array) sum += item;
-
-  return sum / array.length;
-};
-
-console.log(calculateGrade(marks));
+showStarts(10);
