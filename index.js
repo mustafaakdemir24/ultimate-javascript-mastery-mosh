aa919e23fbfacaa5fb5132b470e5c99427ab25ce
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-15 15:13:06 +0300

    Control Flow - Exercise 11-Prime Numbers

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const showStarts = (rows) => {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = "";
-    for (let i = 0; i < row; i++) pattern += "*";
-    console.log(pattern);
+// Prime (whose factors are only 1 and itself)
+// Composite
+
+// 12 = 1, 2, 3, 4, 6, 12
+// Can be divided evenly by its factors
+
+// 11 = 1, 11
+// 13 = 1, 13
+
+const showPrimes = (limit) => {
+  for (let number = 2; number <= limit; number++) {
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) console.log(number);
   }
 };
 
-showStarts(10);
+showPrimes(20);
