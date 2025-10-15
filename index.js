c94f0aad92a2e0085bba4ff0f6d599eb600a79d8
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-15 19:52:19 +0300

    Objects - Factory Functions

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// Object-oriented Programming (OOP)
-
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  isVisible: true,
-  draw: function () {
-    console.log("Draw");
-  },
+// Factory Function
+const createCircle = (radius) => {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
 };
 
-circle.draw(); // Method
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
