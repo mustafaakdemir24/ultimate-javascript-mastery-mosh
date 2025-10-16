51f2df87e99ef10309bb3c909af2ebad613c3a5d
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-16 15:56:49 +0300

    Objects - Enumerating of an Object

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let obj = { value: 10 };
-
-const increase = (obj) => {
-  obj.value++;
+const circle = {
+  radius: 1,
+  draw() {
+    console.log("draw");
+  },
 };
 
-increase(obj);
+for (let key in circle) console.log(key, circle[key]);
 
-console.log(obj);
+for (let key of Object.keys(circle)) console.log(key);
+
+for (let entry of Object.entries(circle)) console.log(entry);
+
+if ("color" in circle) console.log("yes");
