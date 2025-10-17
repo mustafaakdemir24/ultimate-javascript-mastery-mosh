a78090c3e24ab45c4bfb3d7a720b635263319c3e
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-17 14:22:48 +0300

    Objects - Template Literals

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// String primitive
-const message = "This is my\n first message";
+const name = "John";
+const message = "Hi " + name + "\n";
 
-// String object
-const another = new String("hi");
+const another = `Hi ${name} ${2 + 3},
+
+Thank you for joining my mailing list.
+
+Regards,
+Mosh`;
