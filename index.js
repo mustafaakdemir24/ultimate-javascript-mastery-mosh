989b11c6bf385a5da31c2f5571c39397f56cbde0
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-10-23 14:38:21 +0300

    Objects - Exercise 4-Blog Post Object

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// Constructor Function
-function Address(street, city, zipCode) {
-  (this.street = street), (this.city = city), (this.zipCode = zipCode);
-}
+// Blog Post Object
+// title
+// body
+// author
+// views
+// comments
+//    (author, body)
+// isLive
 
-let address1 = new Address("a", "b", "c");
-let address2 = new Address("a", "b", "c");
-let address3 = address1;
-
-const areEqual = (address1, address2) => {
-  return (
-    address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-  );
+const blogPost = {
+  title: "Ultimate JavaScript Course",
+  body: "The ultimate JavaScript course by Mosh Hamedani",
+  author: "Moshfeg Hamedani",
+  views: 11,
+  comments: [
+    {
+      author: "Mustafa Akdemir",
+      body: "Great JavaScript course by Mosh Hamedani",
+    },
+    {
+      author: "a",
+      body: "b",
+    },
+  ],
+  isLive: true,
 };
 
-const areSame = (address1, address2) => {
-  return address1 === address2;
-};
-
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
+console.log(blogPost);
