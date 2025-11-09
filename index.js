00fee0feeb0c451a7c2bc2297583a27fdaf3d199
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-09 15:21:36 +0300

    Arrays - Sorting Arrays (objects)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const numbers = [2, 3, 1];
-numbers.sort();
-console.log(numbers);
+const courses = [
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "javaScript" },
+];
 
-numbers.reverse();
-console.log(numbers);
+courses.sort((a, b) => {
+  // a < b => -1
+  // a > b => 1
+  // a === b => 0
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
+
+  if (nameA > nameB) return 1;
+  if (nameA < nameB) return -1;
+  return 0;
+});
+
+console.log(courses);
