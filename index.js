8ec0748b51962805830489549d4adcc2f2b33088
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-15 12:22:41 +0300

    Arrays - Exercise 4-Moving an Element

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const numbers = [1, 2, 3, 4, 1, 1];
+const numbers = [1, 2, 3, 4];
 
-const except = (array, excluded) => {
-  const output = new Array();
-
-  for (let element of array) {
-    if (!excluded.includes(element)) output.push(element);
+const move = (array, index, offset) => {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.error("Invalid offset");
+    return;
   }
 
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
   return output;
 };
 
-const output = except(numbers, [1, 2, 3]);
+const output = move(numbers, 3, -2);
 
 console.log(output);
