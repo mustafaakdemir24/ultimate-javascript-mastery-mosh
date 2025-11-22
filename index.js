9ceaada85812dd3abf5505e979edf7434e82ad18
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-22 20:30:19 +0300

    Functions - Hoisting

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Function Declaration
+walk();
+
 function walk() {
   console.log("walk");
 }
 
 // Anonymous Function Expression
+run();
+
 let run = function () {
   console.log("run");
 };
-
-let move = run;
-
-run();
-move();
