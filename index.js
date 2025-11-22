f200b0ecb1c3aa5d13d8f3759a1324a5bb7bfdf2
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-22 21:51:17 +0300

    Functions - Default Parameters

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function sum(discount, ...prices) {
-  const total = prices.reduce((a, b) => a + b);
-  return total * (1 - discount);
+function interest(principal, rate = 3.5, years = 5) {
+  return ((principal * rate) / 100) * years;
 }
 
-console.log(sum(0.1, 20, 30));
+console.log(interest(10000));
