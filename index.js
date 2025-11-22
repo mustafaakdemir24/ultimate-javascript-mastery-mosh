0d4a20995b193dee9e8f7ba5506eb2b766d0091b
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-22 20:05:23 +0300

    Functions - Function Declarations vs Expressions

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 },
-];
+// Function Declaration
+function walk() {
+  console.log("walk");
+}
 
-// All the movies in 2018 with rating > 4
-// Sort them by their rating
-// Descending order
-// Pick their title
+// Anonymous Function Expression
+let run = function () {
+  console.log("run");
+};
 
-const titles = movies
-  .filter((m) => m.year === 2018 && m.rating >= 4)
-  .sort((a, b) => a.rating - b.rating)
-  .reverse()
-  .map((m) => m.title);
+let move = run;
 
-console.log(titles);
+run();
+move();
