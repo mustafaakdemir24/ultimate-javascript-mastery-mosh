4b02e912e2092394771f95dd90359c6c28d27a93
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-22 21:26:29 +0300

    Functions - The Rest Operator

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function sum() {
-  let total = 0;
-  for (let value of arguments) total += value;
-  return total;
+function sum(discount, ...prices) {
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
 
-console.log(sum(1, 2, 3, 4, 5, 10));
+console.log(sum(0.1, 20, 30));
