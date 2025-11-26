28d9bfe5a99dac535ed6ba805a9d825703381f05
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-11-26 17:06:52 +0300

    Functions - Local vs Global Scope

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const person = {
-  firstName: "Mosh",
-  lastName: "Hamedani",
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`;
-  },
-  set fullName(value) {
-    if (typeof value !== "string") throw new Error("Value is mot a string.");
+const color = "red";
 
-    const parts = value.split(" ");
-    if (parts.length !== 2) throw new Error("Enter a first and last name.");
-
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  },
-};
-
-try {
-  person.fullName = "";
-} catch (e) {
-  alert(e);
+function start() {
+  const message = "Hi";
+  const color = "blue";
+  console.log(color);
 }
 
-console.log(person);
+function stop() {
+  const message = "stop";
+}
+
+start();
